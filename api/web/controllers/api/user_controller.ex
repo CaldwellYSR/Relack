a05a8bf4be3b173314d01c6ac194b5bc3247ca18
Matthew Caldwell <caldwellysr@gmail.com>
2017-08-01 08:08:01 -0400

    backend authentication finished

---
+++ api/web/controllers/api/user_controller.ex
@@ -0,0 +1,25 @@
+defmodule Relack.UserController do
+  use Relack.Web, :controller
+
+  alias Relack.User
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        new_conn = Guardian.Plug.api_sign_in(conn, user, :access)
+        jwt = Guardian.Plug.current_token(new_conn)
+
+        new_conn
+        |> put_status(:created)
+        |> render(Relack.SessionView, "show.json", user: user, jwt: jwt)
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(Relack.ChangesetView, "error.json", changeset: changeset)
+    end
+
+  end
+
+end
--- api/web/router.ex
+++ api/web/router.ex
@@ -1,11 +1,33 @@
 defmodule Relack.Router do
   use Relack.Web, :router
 
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
+    plug Guardian.Plug.LoadResource
+  end
+
+  scope "/", Relack do
+    pope_through :browser
+
+    get "/", PageController, :index
   end
 
   scope "/api", Relack do
     pipe_through :api
+
+    post "/sessions", SessionController, :create
+    delete "/sessions", SessionController, :delete
+    post "/sessions/refresh", SessionController, :refresh
+    resources "/users", UserCOntroller, only: [:create]
   end
+
 end
